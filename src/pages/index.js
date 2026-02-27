adb420ea514d660a814c5d5c1d09f184fe68072a
d.andreev <dimaandreev0101@gmail.com>
2026-02-27 10:41:41 +0300

    feat: add page logs, add app version

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,10 @@ import "features/mobile-only/components/index.js";
 import "features/add-spends/components/available-spends/index.js";
 import SpendsHistory from "features/spends/services/spendsHistory.js";
 
+const COUNTS_TO_LOG = 10
+
 const main = async () => {
+  let countsToLogI = 0
   await customElements.whenDefined("spends-form");
 
   const spendsFormEl = document.getElementById("spends-form");
@@ -12,6 +15,14 @@ const main = async () => {
     alert("Добавлено 😊");
     SpendsHistory.addItem(value, purpose);
   };
+
+  document.getElementById('app-version').onclick = () => {
+    countsToLogI++
+
+    if (countsToLogI >= COUNTS_TO_LOG) {
+      window.location.href = 'log'
+    }
+  }
 };
 
 main();
